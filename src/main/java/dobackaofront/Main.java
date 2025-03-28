4baabd0195e38df407ddba606d1455729fa662cc
hiroshionishi <hiroshiemarcia@gmail.com>
2025-03-28 09:30:52 -0300

    Aula10POO

---
+++ src/main/java/dobackaofront/Main.java
@@ -0,0 +1,8 @@
+package dobackaofront;
+
+public class Main {
+    public static void main(String[] args) {
+        pessoa p1 = new Pessoa();
+        pessoa p2 = new Pessoa("Hiroshi", "30", 0, "0000");
+    }
+}
---
+++ src/main/java/dobackaofront/Pessoa.java
@@ -0,0 +1,29 @@
+package dobackaofront;
+
+public class Pessoa {
+    //atributos
+    private String nome;
+    private String cpf:
+    private int idade;
+    private String whatsapp;
+
+    //construtores padrao
+    public Pessoa() {
+        nome = "";
+        cpf = "";
+        idade = 0;
+        whatsapp = "";
+    }
+
+    // construtor parametro
+    public Pessoa(String nome, String cpf, int idade, String whatsapp) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.idade = idade;
+        this.whatsapp = whatsapp;
+
+    }
+    //metodos
+
+}
+
